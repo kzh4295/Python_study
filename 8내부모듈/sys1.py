cb62711326ff0c8e7a075dbdd3c185394b2ae414
kzh4295 <kzh4295@naver.com>
2021-03-24 13:53:31 +0900

    깃 프로젝트 제외 완성

--- 8내부모듈/sys1.py
+++ 8내부모듈/sys1.py
@@ -1,10 +1,17 @@
 import sys
+
+#파일만 올렸으니, 파일에서 클리어를 찾아 사용
+import mysys
+mysys.clear()
+
+# 파일에서 clear를 올렸으니 클리어만 바로 사용하면 됨
+from mysys import clear
+clear()
+
 print(sys.argv, len(sys.argv))
 
-sa = sys.argv
-if len(sa) < 2:
-  sys.exit()
+def print_sys_vars():
+  for i in [sys.version, sys.copyright, sys.platform]:
+    print("--->", i)
 
-with open (sa[1], "r", encoding="utf-8") as file:
-  for line in file:
-    print(line)
+sa = sys.argv
